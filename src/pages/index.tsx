ab9eab4c94dbc9e1219422ced73479285746ed63
jasonfzou <forkidd214@gmail.com>
2023-07-23 01:24:05 +0930

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+import { api } from "@/utils/api";
+
+export default function Home() {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>Chat Room</title>
+        <meta name="description" content="Chat Room" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>{hello.data ? hello.data.greeting : "Loading tRPC query..."}</p>
+        <AuthShowcase />
+      </main>
+    </>
+  );
+}
+
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
+
+  console.log({ sessionData });
+
+  return (
+    <div>
+      <p>
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
