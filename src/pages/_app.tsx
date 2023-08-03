34528fec91e1ef9224424c3d0cd94f0570cf5e63
jasonfzou <forkidd214@gmail.com>
2023-08-03 20:57:21 +0930

    feat: add config for progress web app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "@/utils/api";
+import Head from "next/head";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
@@ -11,6 +12,20 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta name="application-name" content="Chatogether" />
+        <meta name="description" content="Let's chat together" />
+        <meta name="theme-color" content="#09090b" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Chatogether" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
